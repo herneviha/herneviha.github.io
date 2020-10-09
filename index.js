74f9282be6612fffee70c9f85996cb618df2cc8f
Topias Saari <saari.topias@gmail.com>
2020-10-09 12:20:22 +0300

    add theme selector

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-window.addEventListener('load', (e) => {
-  fetchMenu().then(restaurants => {
+window.addEventListener("load", (e) => {
+  initThemeChange();
+  fetchMenu().then((restaurants) => {
     renderMenu(restaurants);
   });
 });
 
+function initThemeChange() {
+  var themeLink = document.getElementById("theme");
+  var themeSelector = document.getElementById("theme_selector");
+  console.log(document.getElementById("theme"));
+  themeSelector.addEventListener("click", () => {
+    if (themeLink.attributes.href.value === "theme_1.css") {
+      themeLink.setAttribute("href", "theme_2.css");
+    } else {
+      themeLink.setAttribute("href", "theme_1.css");
+    }
+  });
+}
 const fetchMenu = () => {
   const json = axios
     .get("https://unicafe.fi/wp-json/swiss/v1/restaurants")
@@ -11,7 +24,7 @@ const fetchMenu = () => {
       console.log(error);
     });
   return json;
-}
+};
 
 const renderMenu = (restaurants) => {
   restaurants.data.forEach((restaurant) => {
@@ -23,7 +36,7 @@ const renderMenu = (restaurants) => {
           ).innerHTML += `<section id="${item.date}"><h2 class="date">${item.date}</h2></section>`;
           item.data.forEach((data, i) => {
             if (data.ingredients != "_" && data.ingredients != "") {
-              let ingredients = highlightAllergens(data.ingredients)
+              let ingredients = highlightAllergens(data.ingredients);
               document.getElementById(
                 item.date
               ).innerHTML += `<div class="food_container"><h3 class="name">${data.name}</h3><p class="ingredients">${ingredients}</p></div>`;
@@ -33,41 +46,46 @@ const renderMenu = (restaurants) => {
       });
     }
   });
-}
+};
 
 const notAlreadyPassed = (daydata) => {
   const date = new Date();
-  if (parseInt(daydata.substring(3, 5)) < date.getDate() && parseInt(daydata.substring(6, 8)) === date.getMonth() + 1) {
+  if (
+    parseInt(daydata.substring(3, 5)) < date.getDate() &&
+    parseInt(daydata.substring(6, 8)) === date.getMonth() + 1
+  ) {
     return false;
   } else if (parseInt(daydata.substring(6, 8)) < date.getMonth() + 1) {
     return false;
   }
   return /Ma|Ti|Ke|To|Pe/.test(daydata);
-}
+};
 
 const getSectionId = () => {
   const date = new Date();
-  const weekDays = ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La'];
+  const weekDays = ["Su", "Ma", "Ti", "Ke", "To", "Pe", "La"];
   if (date.getDay() === 6) {
     date.setDate(date.getDate() + 2);
   } else if (date.getDay() === 0) {
     date.setDate(date.getDate() + 1);
   }
-  return `${weekDays[date.getDay()]} ${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.`;
-}
+  return `${weekDays[date.getDay()]} ${date
+    .getDate()
+    .toString()
+    .padStart(2, "0")}.${(date.getMonth() + 1).toString().padStart(2, "0")}.`;
+};
 
 const highlightAllergens = (ingredients) => {
   allergens = /herne|linss(e|i)|pa(v|p)u|pähkin(ä|ö)/;
-  const listOfIngredients = ingredients.split(',');
+  const listOfIngredients = ingredients.split(",");
 
   let result = [];
   for (let i = 0; i < listOfIngredients.length; i++) {
     if (allergens.test(listOfIngredients[i].toLocaleLowerCase())) {
       result.push(`<mark>${listOfIngredients[i]}</mark>`);
-    }
-    else {
+    } else {
       result.push(listOfIngredients[i]);
     }
   }
   return result.join();
-}
+};
